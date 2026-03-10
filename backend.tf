2f8a2f31f64f7b33df06df0b3c39a80dcf59ed17
waghepratiksha21-create <waghepratiksha21@gmail.com>
2026-03-10 16:14:21 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app-virginai"
+    bucket = "manuall-project1"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
