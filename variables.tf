bad2babd3651192e29340715f058963ff2192d77
waghepratiksha21-create <waghepratiksha21@gmail.com>
2026-03-10 13:28:24 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-07ffb2f4d65357b42" 
+    default     = "ami-0b6c6ebed2801a5cb" 
 }
 variable "versioning" {
     type        = bool
